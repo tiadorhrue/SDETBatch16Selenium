a27a1bbbf37721f3a3ebac8f53a68b903c0c1d00
tiadorhrue <tiadivine96@gmail.com>
2023-05-24 17:21:04 -0400

    Selenium Class 03 HW

--- src/test/java/Class03HW/HW02.java
+++ src/test/java/Class03HW/HW02.java
@@ -8,13 +8,16 @@ import org.openqa.selenium.WebElement;
 import static Utlis.CommonMethods.driver;
 
 public class HW02 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         CommonMethods.openBrowserAndLaunchApplication("http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login%E2%80%9D");
         WebElement userName = driver.findElement(By.cssSelector("input[name=txtUsername"));
         userName.sendKeys("Admin");
         WebElement logIn = driver.findElement(By.cssSelector("input[type=submit]"));
         logIn.click();
+        Thread.sleep(4000);
+        driver.close();
+
 
 
     }
